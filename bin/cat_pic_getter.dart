fa1ee67ab1a839e9407cce2aa79fe3e6aa1270cc
Dazhyar <dazhyarzanyar@gmail.com>
2024-11-08 00:37:29 +0300

    init commit

---
+++ bin/cat_pic_getter.dart
@@ -0,0 +1,6 @@
+import 'package:http/http.dart';
+
+void main() {
+    Uri url = Uri.https('cataas.com', '/cat');
+    print(url);
+}
---
+++ test/cat_pic_getter_test.dart
@@ -0,0 +1,8 @@
+import 'package:cat_pic_getter/cat_pic_getter.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
